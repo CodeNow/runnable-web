a777e4c0635a8272ac38ded2718af1907e1d9e12
tjmehta <tejesh.mehta@gmail.com>
2013-06-17 11:56:36 -0700

    span 9 to code-editor

--- app/views/code.js
+++ app/views/code.js
@@ -1,6 +1,6 @@
 var BaseView = require('./base_view');
 module.exports = BaseView.extend({
-  className: 'code-editor',
+  className: 'code-editor span9',
   minHeight: 300,
   maxHeight: 550,
   postRender: function () {
