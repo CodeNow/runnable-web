00fa170411828231514476ed523279c2ed85f10c
Enos Feedler <jeff.kingyens@gmail.com>
2013-06-16 20:54:33 -0700

    test tests

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ require('console-trace')({ always:true });
 var config = require('./server/lib/env').current;
 var server = require('./server/server');
 
-console.log(config.port);
-var port = process.env.PORT || config.port;
+var port = config.port;
 
 server.init({}, function(err) {
   if (err) throw err;
--- test/user.js
+++ test/user.js
@@ -5,7 +5,7 @@ describe('User', function() {
 
   it('should use login as the model id', function() {
     var user = new User({login: 'someusername'});
-    user.id.should.equal('someusername');
+    user.id.should.equal('me');
   });
 
 });
