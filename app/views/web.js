6a21a2a6ff2e9a6f1635dbb19d1862a69578e69b
prafulrana <prafulrana@gmail.com>
2013-07-11 13:08:21 -0700

    Run working

--- app/views/web.js
+++ app/views/web.js
@@ -2,6 +2,7 @@ var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
   postRender: function () {
+    console.log("http://" + this.model.get("docker_id") + "." + "runnableapp.dev");
     $('.web-iframe').attr('src', "http://" + this.model.get("docker_id") + "." + "runnableapp.dev");
     $('.project-tail-iframe').attr('src', "http://" + this.model.get("logs"));
   }
