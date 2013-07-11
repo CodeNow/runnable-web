1c9d6e995b518681bd1b969e6d46c9ec3adf72f3
prafulrana <prafulrana@gmail.com>
2013-07-10 21:24:40 -0700

    Tags fully working in Publish mode

--- app/views/runnable_new_tag_form.js
+++ app/views/runnable_new_tag_form.js
@@ -1,6 +1,7 @@
 var BaseView = require('./base_view');
-var Base = require('../models/base')
-
+var BaseModel = require('../models/base')
+var BaseCollection = require('../collections/base')
+var utils = require('../utils');
 
 
 module.exports = BaseView.extend({
@@ -8,23 +9,38 @@ module.exports = BaseView.extend({
   events: {
     'submit' : 'addNewTag'
   },
-  postRender: function () {
-    this.model.on('change', function () {
-      this.render();
-    }, this);
+  postHydrate: function () {
+    this.collection = new BaseCollection(this.model.get('tags'), {
+      app: this.app,
+      url:'/users/me/runnables/'+this.model.id+'/tags'
+    });
+    this.listenTo(this.collection, 'add remove reset', this.render.bind(this));
+    this.listenTo(this.model, 'change:tags', this.onChange.bind(this));
+  },
+  onChange: function () {
+    this.collection.reset(this.model.get('tags'));
   },
   addNewTag: function (evt) {
     evt.preventDefault();
     evt.stopPropagation();
-
-    var newTagModel = new Base({name:this.$(".tag-form-text").val()}, {app:this.app});
-    newTagModel.save({}, {
-      url:'/users/me/runnables/' + this.model.id + '/tags',
-      method: 'POST'
-    });
-    // var tag = new Backbone.Model({})
-    // tags.save({}, {url:…, method:'PUT'}
-    // this.model.save({tags : currentTags}, {});
+    var formData = $(evt.currentTarget).serializeObject();
+    var collection = this.collection;
+    var tag = new BaseModel(formData, {app:this.app});
+    var options = utils.successErrorToCB(saveCallback.bind(this));
+    options.method = 'POST';
+    // assume success
+    collection.add(tag);
+    tag.save({}, options);
+    this.model.set('tags', collection.toJSON());
+    this.$('input').val('');
+    function saveCallback (err, tag) {
+      if (err) {
+        this.showError(err);
+        collection.remove(tag);
+        this.model.set('tags', collection.toJSON());
+        this.$('input').val(tag.get('name'));
+      }
+    }
   }
 });
 
