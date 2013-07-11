ffee10fefa5bc3a968f0213d858bd0f3e9aebb4a
prafulrana <prafulrana@gmail.com>
2013-07-11 15:16:39 -0700

    Projet tail log support

--- app/views/web.js
+++ app/views/web.js
@@ -2,9 +2,8 @@ var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
   postRender: function () {
-    console.log("http://" + this.model.get("docker_id") + "." + "runnableapp.dev");
     $('.web-iframe').attr('src', "http://" + this.model.get("docker_id") + "." + "runnableapp.dev");
-    $('.project-tail-iframe').attr('src', "http://" + this.model.get("logs"));
+    $('.project-tail-iframe').attr('src', "http://logs.runnableapp.dev/log.html?termId=" + this.model.get("token"));
   }
 });
 
