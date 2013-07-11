727b24c9d8e9ba2e00cd917dff28de175fea88d3
prafulrana <prafulrana@gmail.com>
2013-07-10 19:47:36 -0700

    Tagging working and saving

--- app/views/runnable_new_tag_form.js
+++ app/views/runnable_new_tag_form.js
@@ -1,12 +1,30 @@
 var BaseView = require('./base_view');
+var Base = require('../models/base')
+
+
 
 module.exports = BaseView.extend({
 	tagName: 'form',
   events: {
-    'click .icon-plus' : 'addNewTag'
+    'submit' : 'addNewTag'
+  },
+  postRender: function () {
+    this.model.on('change', function () {
+      this.render();
+    }, this);
   },
   addNewTag: function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
 
+    var newTagModel = new Base({name:this.$(".tag-form-text").val()}, {app:this.app});
+    newTagModel.save({}, {
+      url:'/users/me/runnables/' + this.model.id + '/tags',
+      method: 'POST'
+    });
+    // var tag = new Backbone.Model({})
+    // tags.save({}, {url:…, method:'PUT'}
+    // this.model.save({tags : currentTags}, {});
   }
 });
 
