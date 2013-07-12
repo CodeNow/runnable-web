7248b1d11d929a80ed05e253568ce06f6dbee318
tjmehta <tejesh.mehta@gmail.com>
2013-07-11 19:58:36 -0700

    missing new files

---
+++ app/views/runnable/new.js
@@ -0,0 +1,19 @@
+var BaseView = require('../base_view');
+
+module.exports = BaseView.extend({
+	tagName: 'section',
+  className: 'content',
+  events: {
+    'submit form' : 'submit'
+  },
+  submit: function (evt) {
+    evt.preventDefault();
+    var runnableId = $(evt.currentTarget).serializeObject().id;
+    if (runnableId.length === 16) // TODO: for now
+      window.location.href = '/new/'+runnableId;
+    else
+      this.showError('Invalid Runnable Id. Double check the url of the project you are trying to fork.');
+  }
+});
+
+module.exports.id = "runnable/new";
