c1a2c70462369d41d20b124dd5e0f76652c1ccd8
generalhenry <htilford@gmail.com>
2013-07-13 10:37:30 -0700

    debug

--- app/views/web.js
+++ app/views/web.js
@@ -2,6 +2,7 @@ var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
   postRender: function () {
+  	debugger;
     $('.web-iframe').attr('src', "http://" + this.model.get("docker_id") + "." + "runnableapp.dev");
     $('.project-tail-iframe').attr('src', "http://logs.runnableapp.dev/log.html?termId=" + this.model.get("token"));
   }
