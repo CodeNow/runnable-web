e85dd595ac45141a3343a8ef3219f28c97f28e12
generalhenry <htilford@gmail.com>
2013-07-13 10:40:49 -0700

    fix

--- app/views/terminal.js
+++ app/views/terminal.js
@@ -3,7 +3,7 @@ var BaseView = require('./base_view');
 module.exports = BaseView.extend({
   postRender: function () {
     console.log("Terminals model is", this.model);
-    $('.terminal-iframe').attr('src', "http://terminals.runnableapp.dev/term.html?termId=" + this.model.get("token"));
+    $('.terminal-iframe').attr('src', "http://terminals." + this.app.get('domain') + "/term.html?termId=" + this.model.get("token"));
   }
 });
 
--- app/views/web.js
+++ app/views/web.js
@@ -2,9 +2,8 @@ var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
   postRender: function () {
-  	debugger;
-    $('.web-iframe').attr('src', "http://" + this.model.get("docker_id") + "." + "runnableapp.dev");
-    $('.project-tail-iframe').attr('src', "http://logs.runnableapp.dev/log.html?termId=" + this.model.get("token"));
+    $('.web-iframe').attr('src', "http://" + this.model.get("docker_id") + "." + this.app.get('domain'));
+    $('.project-tail-iframe').attr('src', "http://logs." + this.app.get('domain') + "/log.html?termId=" + this.model.get("token"));
   }
 });
 
