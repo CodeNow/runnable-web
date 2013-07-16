b9e0db0d61a406c126854d08171af593945fe836
generalhenry <htilford@gmail.com>
2013-07-16 15:15:02 -0700

    debug

--- app/views/web.js
+++ app/views/web.js
@@ -1,6 +1,10 @@
 var BaseView = require('./base_view');
 var _ = require('underscore');
 
+window.addEventListener("message", function (event) {
+  alert('message:' + event.data);
+}, false);
+
 module.exports = BaseView.extend({
   id: 'web',
   postInitialize: function () {
