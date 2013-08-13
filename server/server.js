8f31cfd2c9d1d2028721614f055940f7cf6cae59
Tejesh Mehta <tj@init.me>
2013-08-13 12:49:34 -0700

    removed livereload from app (moved to grunt)

--- server/server.js
+++ server/server.js
@@ -24,21 +24,6 @@ redisStore = connectRedis(express);
 
 app = express();
 
-// var liveReloadPort = 35731;
-// app.configure('development', function () {
-//   var mergedCSSPath   = 'public/styles/index.css';
-//   // Create a live reload server instance
-//   var lrserver = require('tiny-lr')();
-//   // Listen on port 35729
-//   lrserver.listen(liveReloadPort, function(err) { console.log('LR Server Started'); });
-//   // Then later trigger files or POST to localhost:35729/changed
-//   lrserver.changed({body:{files:[
-//     'public/mergedAssets.js',
-//     mergedCSSPath,
-//     'public/images/*.*'
-//   ]}});
-// });
-
 //
 // Initialize our server
 //
@@ -104,9 +89,9 @@ function initMiddleware() {
   app.use(express.logger());
   app.use(express.bodyParser());
 
-  // app.configure('development', function() {
-  //   app.use(require('./middleware/liveReload')({port:liveReloadPort}));
-  // });
+  app.configure('development', function() {
+    app.use(require('./middleware/liveReload')({port:process.env.LIVERELOAD_PORT}));
+  });
 
   app.use(app.router);
   app.use(mw.errorHandler());
