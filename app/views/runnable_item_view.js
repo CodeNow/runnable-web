7a733fc1ff3cf41150c11d4417ca55d4c05fe7a1
tjmehta <tj@init.me>
2013-09-02 18:59:36 -0700

    fixed related links for seo

--- app/views/runnable_item_view.js
+++ app/views/runnable_item_view.js
@@ -5,6 +5,11 @@ var _super = BaseView.prototype;
 module.exports = BaseView.extend({
   tagName: 'li',
   getTemplateData: function () {
+    var model = this.options.model;
+    model.virtuals = {
+      nameWithTags: model.nameWithTags(true),
+      appURL: model.appURL
+    };
     return this.options;
   }
 });
