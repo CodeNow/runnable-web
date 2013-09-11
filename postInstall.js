36f7a1b8964f53bbdf08d3d8bd49000f7bca64e4
tjmehta <tj@init.me>
2013-09-10 19:58:41 -0700

    readded postInstall to keep rendr up to date

--- postInstall.js
+++ postInstall.js
@@ -22,38 +22,36 @@ d.on('error', function (err) {
   console.error('NPM postinstall ERROR!! ', err.stack || err.message);
 });
 d.run(function () {
-  exec(getRendrCommit, i(function (commitHash) {
-    commitHash = commitHash.trim();
-    if (commitHash == rendrCommit) {
-      log('POSTINSTALL: Rendr is up-to-date (%s)', commitHash);
-    }
-    else {
-      log('new commit found '+commitHash);
-      squashRendr(function () {
-        installRendr(writeCommitFile);
-      });
-    }
-    function squashRendr (cb) {
-      log('squashing rendr');
-      rimraf(path.join(__dirname, 'node_modules', 'rendr'), i(cb));
-    }
-    function installRendr (cb) {
-      log('installing rendr');
-      var packages = ['rendr'].map(function(dep) {
-        var pkgDep = pkg.dependencies[dep];
-        return (~pkgDep.indexOf('://')) ?
-          pkgDep : // If it has a protocol, assume it's a link to a repo.
-          dep + '@' + pkgDep;
-      });
-      npm.load({cwd:__dirname}, i(function () {
-        npm.commands.install(packages, i(cb));
-      }));
-    }
-    function writeCommitFile () {
-      log('installed '+commitHash);
-      fs.writeFile('./.rendr-commit', commitHash, i(function(message) {
-        log("POSTINSTALL: Rendr updated (%s)", commitHash);
-      }));
-    }
-  }));
+  var commitHash = pkg.dependencies['rendr'].split('#')[1];
+  if (commitHash == rendrCommit) {
+    log('POSTINSTALL: Rendr is up-to-date (%s)', commitHash);
+  }
+  else {
+    log('new commit found '+commitHash);
+    squashRendr(function () {
+      installRendr(writeCommitFile);
+    });
+  }
+  function squashRendr (cb) {
+    log('squashing rendr');
+    rimraf(path.join(__dirname, 'node_modules', 'rendr'), i(cb));
+  }
+  function installRendr (cb) {
+    log('installing rendr');
+    var packages = ['rendr'].map(function(dep) {
+      var pkgDep = pkg.dependencies[dep];
+      return (~pkgDep.indexOf('://')) ?
+        pkgDep : // If it has a protocol, assume it's a link to a repo.
+        dep + '@' + pkgDep;
+    });
+    npm.load({cwd:__dirname}, i(function () {
+      npm.commands.install(packages, i(cb));
+    }));
+  }
+  function writeCommitFile () {
+    log('installed '+commitHash);
+    fs.writeFile('./.rendr-commit', commitHash, i(function(message) {
+      log("POSTINSTALL: Rendr updated (%s)", commitHash);
+    }));
+  }
 });
