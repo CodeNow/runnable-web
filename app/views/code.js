f4b564b380eb80a93eec0014a4573e1eaf484ee0
prafulrana <prafulrana@gmail.com>
2013-09-13 12:28:38 -0700

    add user id to file open erros so we can track Publishers. https://rollbar.com/item/272323760/instance/395866192/ works

--- app/views/code.js
+++ app/views/code.js
@@ -65,7 +65,7 @@ module.exports = BaseView.extend({
   _createSession: function (file) {
     if (!utils.exists(file.get('content'))) { //TODO:  content null.. right now content null does not mean unfetched it means unsupported
       alert('This file format is not supported by our editor.');
-      _rollbar.push({level: 'error', msg: "Couldn't open client requested file", errMsg: file.get('name')});
+      _rollbar.push({level: 'error', msg: "Couldn't open client requested file", errMsg: {name:file.get('name'), user:this.app.user.id}});
       this.file = null;
       file.trigger('close:file', file);
     }
