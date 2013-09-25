2b5edbd500c828d7db68bce1af95fc2be10300b6
tjmehta <tj@init.me>
2013-09-24 21:50:41 -0700

    term/tail loader

--- app/views/tail.js
+++ app/views/tail.js
@@ -3,14 +3,13 @@ var Super = BaseView.prototype;
 
 module.exports = BaseView.extend({
   id: 'output-terminal-container',
-  className: 'resizable-iframe',
+  className: 'resizable-iframe loading',
   getTemplateData: function () {
     this.options.url = "http://" + this.model.get("servicesToken")+ "." + this.app.get('domain') + "/static/log.html";
     return this.options;
   },
   postRender: function () {
     this.$iframe = this.$('iframe');
-    this.loading(true);
     this.$iframe.load(this.loading.bind(this, false));
   }
 });
