734dbd0cf015cbc8d4a58102db4b8a59e1c315d2
tjmehta <tj@init.me>
2013-10-04 13:50:10 -0700

    global link click tracking

--- app/views/app_view.js
+++ app/views/app_view.js
@@ -1,4 +1,5 @@
 var BaseAppView = require('rendr/shared/base/app_view');
+var Super = BaseAppView.prototype;
 
 var $body = $('body');
 
@@ -7,5 +8,13 @@ module.exports = BaseAppView.extend({
     this.app.on('change:loading', function(app, loading) {
       $body.toggleClass('loading', loading);
     }, this);
-  }
+  },
+  _interceptClick: function(e) {
+    var properties = {}
+    if (e.currentTarget) {
+      properties.href =  $(e.currentTarget).attr('href');
+    }
+    Track.event('App', 'Click Link', properties);
+    Super._interceptClick.apply(this, arguments);
+  },
 });
