dd9c0e55e4b268b80709291051fc438be9227d2e
nommer <tli415@gmail.com>
2013-10-24 12:29:04 -0700

    Added run options popover

--- app/views/runnable/index.js
+++ app/views/runnable/index.js
@@ -2,7 +2,22 @@ var BaseView = require('../base_view');
 var _ = require('underscore');
 
 module.exports = BaseView.extend({
-
+  className: 'main-controls',
+  events: {
+    'click .run-options' : 'runOptions'
+  },
+  postRender: function () {
+    this.$runOptions = this.$('.run-options');
+    this.$runOptions.popover({
+      title: '<button type="button" class="close">×</button><h4>Run Options</h4>',
+      content: '<form class="form-group"><input class="form-control" required><button class="green">Run</button></form><hr><form class="form-group"><input class="form-control" required><button class="green">Run &amp; Build</button></form></div></div>',
+      html: true,
+      placement: 'bottom'
+    });
+  },
+  runOptions: function (evt) {
+    this.$runOptions.toggleClass('active');
+  }
 });
 
 module.exports.id = "runnable/index";
