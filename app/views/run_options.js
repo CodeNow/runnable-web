5bd3a8de44926d5f2e3f01d4c58a1b43eeb290c4
tjmehta <tj@init.me>
2013-10-29 17:46:57 -0700

    run options popover functionality

---
+++ app/views/run_options.js
@@ -0,0 +1,91 @@
+var BaseView = require('./base_view');
+var utils = require('../utils');
+
+module.exports = BaseView.extend({
+  tagName: 'button',
+  className: 'btn green run-options',
+  events: {
+    'click'        : 'togglePopover',
+  },
+  postRender: function () {
+    // the popover makes this view kind of message logic wise..
+    this.$el.popover({
+      title: '<button type="button" class="close">×</button><h4>Run Options</h4>',
+      content: this.$('.content-template').html(),
+      html: true,
+      placement: 'bottom'
+    });
+    this.$el.on('shown.bs.popover', this.attachPopoverEvents.bind(this));
+    this.$el.on('hide.bs.popover', this.removePopoverEvents.bind(this));
+  },
+  attachPopoverEvents: function (evt) {
+    var $popover = this.getPopover();
+    if ($popover) {
+      $popover.find('input').on('change', this.updateRunOption.bind(this));
+      $popover.find('form').on('submit', this.submitRunOption.bind(this));
+      $popover.find('.close').on('click', this.closePopover.bind(this));
+    }
+  },
+  removePopoverEvents: function (evt) {
+    var $popover = this.getPopover();
+    if ($popover) {
+      $popover.find('input').off('change');
+      $popover.find('form').off('submit');
+      $popover.find('.close').off('click');
+    }
+  },
+  getPopover: function () {
+    var $popover = this.$el.next();
+    return $popover.hasClass('popover') ? $popover : null;
+  },
+  togglePopover: function () {
+    this.$el.toggleClass('active');
+  },
+  closePopover: function () {
+    this.$el
+      .popover('hide')
+      .removeClass('active');
+  },
+  updateRunOption: function (evt) {
+    var $input = $(evt.currentTarget);
+    var data = {};
+    data[$input.attr('name')] = $input.val()
+    var opts = utils.cbOpts(callback, this);
+    this.model.save(data, opts);
+    function callback (err) {
+      if (!this.getPopover()) return;
+      if (err) {
+        this.showError(err);
+      }
+      else {
+        this.saveEffect($input);
+      }
+    }
+  },
+  submitRunOption: function (evt) {
+    var $form = $(evt.currentTarget);
+    var data = $form.serializeObject();
+    var opts = utils.cbOpts(callback, this);
+    this.model.save(data, opts);
+    function callback (err) {
+      if (!this.getPopover()) return; // if popover has been closed
+      if (err) {
+        this.showError(err);
+      }
+      else {
+        this.saveEffect($form.find('input'));
+      }
+    }
+  },
+  saveEffect: function ($inputs) {
+    $inputs.each(function () {
+      var $save = $(this).next();
+      var showTime = 800;
+      $save.fadeIn(function () {
+        setTimeout($save.fadeOut.bind($save), showTime);
+      });
+    });
+  }
+});
+
+module.exports.id = "RunOptions";
--- app/views/runnable/index.js
+++ app/views/runnable/index.js
@@ -2,39 +2,7 @@ var BaseView = require('../base_view');
 var _ = require('underscore');
 
 module.exports = BaseView.extend({
-  className: 'main-controls',
-  events: {
-    'click .run-options' : 'runOptions',
-    'click .close' : 'runOptionsClose',
-    'blur input' : 'runOptionsSave',
-  },
-  postRender: function () {
-    this.$runOptions = this.$('.run-options');
-    this.$runOptions.popover({
-      title: '<button type="button" class="close">×</button><h4>Run Options</h4>',
-      content: '<form class="form-group form-horizontal clearfix"><label for="save-run">Run cmd</label><input class="form-control" id="save-run" required><div class="saved">Saved</div></form><hr><form class="form-group form-horizontal clearfix"><label for="save-build">Build cmd</label><input class="form-control" id="save-build" required><div class="saved">Saved</div></form></div></div>',
-      html: true,
-      placement: 'bottom'
-    });
-  },
-  runOptions: function () {
-    this.$runOptions.toggleClass('active');
-  },
-  runOptionsClose: function () {
-    this.$runOptions
-      .popover('hide')
-      .removeClass('active');
-  },
-  runOptionsSave: function (evt) {
-    this.$thisInput = this.$(evt.target);
 
-    if (this.$thisInput[0].value) {
-      this.$thisInput.siblings('.saved').fadeIn();
-    }
-    setTimeout(function(){
-      this.$('.saved').stop().fadeOut();
-    },1000);
-  }
 });
 
 module.exports.id = "runnable/index";
