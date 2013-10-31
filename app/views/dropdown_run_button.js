3f65ad1d4d2e23cabd991c541933fa6f682617aa
tjmehta <tj@init.me>
2013-10-30 20:26:06 -0700

    run cmd and build cmd saving after popover ui changes

--- app/views/dropdown_run_button.js
+++ app/views/dropdown_run_button.js
@@ -3,19 +3,10 @@ var BaseView = require('./base_view');
 module.exports = BaseView.extend({
   className: 'btn-group',
   events: {
-    'click .run-options' : 'togglePopover',
-    'blur input'         : 'saveEffect'
+    'click .run-options' : 'togglePopover'
   },
   togglePopover: function () {
     $('.popover').toggleClass('in');
-  },
-  saveEffect: function (evt) {
-    var $input = $(evt.currentTarget);
-    var $save = $input.next();
-    $save.addClass('in');
-    $input.on('focus',function(){
-      $save.removeClass('in');
-    });
   }
 });
 
--- app/views/run_popover.js
+++ app/views/run_popover.js
@@ -1,13 +1,75 @@
 var BaseView = require('./base_view');
+var utils = require('../utils');
 
 module.exports = BaseView.extend({
   tagName: 'div',
   className: 'popover fade bottom',
-  show: function (el) {
+  events: {
+    'submit form'  : 'submitRunOption',
+    'change input' : 'updateRunOption'
+  },
+  hidden: function () {
+    return !this.$el.hasClass('in');
+  },
+  show: function () {
     this.$el.addClass('in');
   },
   hide: function () {
     this.$el.removeClass('');
+  },
+  updateRunOption: function (evt) {
+    var $input = $(evt.currentTarget);
+    var data = {};
+    data[$input.attr('name')] = $input.val();
+    console.log(data);
+    var opts = utils.cbOpts(callback, this);
+    this.model.save(data, opts);
+    function callback (err) {
+      debugger;
+      if (this.hidden()) return;
+      if (err) {
+        this.showError(err);
+      }
+      else {
+        this.showSave($input);
+      }
+    }
+  },
+  submitRunOption: function (evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var $form = $(evt.currentTarget);
+    var data = $form.serializeObject();
+    console.log(data);
+    var opts = utils.cbOpts(callback, this);
+    this.model.save(data, opts);
+    function callback (err) {
+      debugger;
+      if (this.hidden()) return;
+      if (err) {
+        this.showError(err);
+      }
+      else {
+        this.saveEffect($form.find('input'));
+      }
+    }
+  },
+  saveEffect: function ($inputs) {
+    var self = this;
+    $inputs.each(function () {
+      var $input = $(this);
+      self.showSave($input);
+      $input.on('focus', self.focusInput);
+    });
+  },
+  focusInput: function (evt) {
+    this.hideSave($(evt.currentTarget));
+  },
+  showSave: function ($input) {
+    $input.siblings('.saved').addClass('in');
+  },
+  hideSave: function ($input) {
+    $input.siblings('.saved').removeClass('in');
   }
 });
 
