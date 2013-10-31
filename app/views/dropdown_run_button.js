0b3985457cd9f1cee4a4de13e55210449069f031
nommer <tli415@gmail.com>
2013-10-30 18:03:19 -0700

    non-bootstrap popover

--- app/views/dropdown_run_button.js
+++ app/views/dropdown_run_button.js
@@ -3,10 +3,19 @@ var BaseView = require('./base_view');
 module.exports = BaseView.extend({
   className: 'btn-group',
   events: {
-    'click run-options' : 'togglePopover'
+    'click .run-options' : 'togglePopover',
+    'blur input'         : 'saveEffect'
   },
   togglePopover: function () {
-
+    $('.popover').toggleClass('in');
+  },
+  saveEffect: function (evt) {
+    var $input = $(evt.currentTarget);
+    var $save = $input.next();
+    $save.addClass('in');
+    $input.on('focus',function(){
+      $save.removeClass('in');
+    });
   }
 });
 
