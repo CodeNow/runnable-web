691d4276d7b96f0d9ccde254e31a7670e755b210
nommer <tli415@gmail.com>
2013-11-18 15:11:26 -0800

    placeholder with tooltips

--- app/views/runnable_item_view.js
+++ app/views/runnable_item_view.js
@@ -8,6 +8,11 @@ module.exports = BaseView.extend({
   events: {
     'click .delete': 'deleteRunnable'
   },
+  postHydrate: function () {
+    this.$('[data-toggle=tooltip]').tooltip({
+      placement: 'bottom'
+    });
+  },
   getTemplateData: function () {
     this.options.user = this.app.user;
     return this.options;
