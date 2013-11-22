4454b9b37ad6d773c9eb470562cfd019b90de01f
generalhenry <htilford@gmail.com>
2013-11-21 17:28:31 -0800

    show on refresh

--- app/views/publish_loader.js
+++ app/views/publish_loader.js
@@ -5,35 +5,11 @@ module.exports = BaseView.extend({
   id: 'publish-loader',
   className: 'overlay-loader',
   postRender: function () {
-    // listen to events
-    // on event 1 call event1
-
-    var i = 1, self = this;
+    var i = 1, self = this, status = this.model.get('status');
+    if (status !== 'Editing' && status !== 'Draft') {
+      this.$el.show();
+    }
     this.$('h1:first-child').addClass('in');
-    // function testLoop(){
-    //   setTimeout(function(){
-    //     this.$('h1.in').prop('class','out');
-    //     this.$('h1:nth-child(' + i + ')').addClass('in');
-    //     i++;
-    //     if (i < 5) {
-    //       testLoop();
-    //     } else if (i === 5) {
-    //       //sim error on last step
-    //       setTimeout(function(){
-    //         this.$('#progress-error').addClass('in');
-    //       },4000);
-    //     }
-    //   },4000);
-    // };
-    // $('#pubwarn-back-button, #pubwarn-new-button').on('click',function(){
-    //   testLoop();
-    // });
-    // //restart on cancel
-    // this.$('#progress-steps > a').on('click',function(){
-    //   $('#publish-loader').hide();
-    //   $('h1, #progress-error').removeClass('out in')
-    //   i = 1;
-    // });
 
     var primus = new Primus('http://cybertron.' + this.app.get('domain'), { 
       transformer: 'engine.io' 
@@ -68,7 +44,6 @@ module.exports = BaseView.extend({
       if (err) {
         cb(err);
       } else {
-        console.log('COMMIT INIT SUCCESS');
         this.$el.show();
       }
     }
