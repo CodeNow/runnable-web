4c2bcd8f21d86b563d1af20209063aff0eaab4f6
nommer <tli415@gmail.com>
2013-12-10 17:00:47 -0800

    consistent tag styles

--- app/views/tag_link.js
+++ app/views/tag_link.js
@@ -2,6 +2,7 @@ var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
   tagName: 'a',
+  className: 'tag',
   preRender: function () {
     var name = this.options.name;
     this.attributes= {
