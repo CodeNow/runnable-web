a31a6f1b28a381b1d6336dc8efed56361376da35
nommer <tli415@gmail.com>
2013-12-13 15:26:42 -0800

    border radius and tabs

--- app/views/dashboard_runnables.js
+++ app/views/dashboard_runnables.js
@@ -8,11 +8,7 @@ module.exports = BaseView.extend({
     this.listenTo(this.collection, 'add remove', this.render.bind(this));
   },
   preRender: function () {
-    if (this.options.editmode) {
-      this.className = 'dashboard-runnables tabs';
-    } else {
-      this.className = 'dashboard-runnables';
-    }
+    this.className = this.options.classname;
   },
   getTemplateData: function () {
     this.options.isimage = this.collection instanceof require('../collections/images');
