9ad6a4b022a143622c0903e24f51ca6b0d81716c
nommer <tli415@gmail.com>
2014-01-03 14:37:54 -0800

    inline validation for titles

--- app/views/runnable_title.js
+++ app/views/runnable_title.js
@@ -40,26 +40,18 @@ module.exports = BaseView.extend({
   submitName: function (evt) {
     evt.preventDefault();
     var formData = $(evt.currentTarget).serializeObject();
-    this.options.editmode = false; // assume success, change will rerender
+    // this.options.editmode = false; // assume success, change will rerender
     if (formData.name === this.model.get('name')) this.render();
     var options = utils.cbOpts(cb, this);
     this.model.save(formData,  options);
 
     function cb (err) {
       if (err === 'a shared runnable by that name already exists') {
-        var actionHandler = function(dialogItself){
-          dialogItself.close();
-        };
-        this.showPrompt({
-          message:
-            '<p>Choose a unique name for your project.<br><strong>Basic Java Example</strong> is taken.'+
-            '<input type="text" class="form-control" required>',
-          actionLabel: 'Save Title',
-          actionHandler: actionHandler
-        });
+        this.$('form').append('<div class="alert alert-warning"><strong>That name is taken!</strong> Try something else.</div>');
       } else if (err) {
-        this.setEditMode(true);
         this.showError(err);
+      } else {
+        this.setEditMode(false);
       }
     }
   }
