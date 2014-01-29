20cbfea4623b4d535051f08bc75d501e5b02cdc3
nommer <tli415@gmail.com>
2014-01-28 16:47:09 -0800

    git card

--- app/views/channel_cards.js
+++ app/views/channel_cards.js
@@ -5,11 +5,29 @@ module.exports = BaseView.extend({
   tagName: 'section',
   id: 'cards',
   className: 'container',
+  events: {
+    'input input' : 'enableSelect',
+    'change select' : 'changeLanguage'
+  },
   getTemplateData: function () {
     if (this.options.basehref == '/new') {
       this.collection.insert(0, new Channel({ name:'runnableid' }));
     }
     return this.options;
+  },
+  enableSelect: function (evt) {
+    var select = this.$('select');
+
+    if ($(evt.currentTarget)[0].value) {
+      select.prop('disabled',false);
+    } else {
+      select.prop('disabled',true);
+    }
+  },
+  changeLanguage: function () {
+    var langVal = this.$('select')[0].value;
+
+    this.$('.dropdown').text(langVal);
   }
 });
 
