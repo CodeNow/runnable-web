2253fc83e6ecc0e3e9af2c92da20ac8216ed322e
nommer <tli415@gmail.com>
2014-02-03 18:36:04 -0800

    clean up

--- app/views/file_select.js
+++ app/views/file_select.js
@@ -1,22 +1,18 @@
 var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
-  id: "project-select-tabs",
-  className: "dropdown alt",
+  id: 'project-select-tabs',
+  className: 'dropdown',
   events: {
-    'change select':'selectChanged'
+    'change select' : 'changeSelect'
   },
-  selectChanged: function (evt) {
-    // evt.preventDefault();
-    var selectedIndex = $(".file_select_menu_class")[0].selectedIndex;
-    this.collection.at(selectedIndex).set("selected", true);
-    // this.render();
-  },
-  preRender: function () {
+  changeSelect: function () {
+    var selectedIndex = this.$('select')[0].selectedIndex;
+    this.collection.at(selectedIndex).set('selected', true);
   },
   postHydrate: function () {
     this.listenTo(this.collection, 'change', this.render.bind(this));
   }
 });
 
-module.exports.id = "FileSelect";
+module.exports.id = 'FileSelect';
