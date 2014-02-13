eb648caf0249ec22a6ea88aeadee44a6161febc6
Casey Flynn <cflynn.us@gmail.com>
2014-02-13 12:27:16 -0800

    PC-28 removing comment block

--- app/views/create_new_button.js
+++ app/views/create_new_button.js
@@ -7,38 +7,6 @@ module.exports = BaseView.extend({
     href: '/new'
   }
 
- /**
-  * Disabling signup modal for unauthenticated users...
-  */
- /*
-  events: {
-    click: 'click'
-  },
-  click: function (evt) {
-
-    if (this.app.user.isRegistered()) {
-      // let the link work...
-    }
-    else {
-      evt.stopPropagation();
-      evt.preventDefault();
-      this.openLogin();
-    }
-
-  },
-  openLogin: function () {
-    var user = this.app.user;
-    var router = this.app.router;
-    var href = this.attributes.href;
-    var signupModal = new SignupModal({
-      app    : this.app,
-      onClose: this.stopListening.bind(this, user)
-    });
-    this.listenToOnce(user, 'change:username', router.navigate.bind(router, href, {trigger:true}));
-    signupModal.open();
-  }
-  */
-
 });
 
 module.exports.id = "CreateNewButton";
