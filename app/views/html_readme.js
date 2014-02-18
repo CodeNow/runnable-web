bdf957562ce2d0d679d3da6d8cdb71fa8bbbe328
Casey Flynn <cflynn.us@gmail.com>
2014-02-18 12:40:22 -0800

    PC-48 case-insensitive determination of proper README.md file to display

--- app/views/html_readme.js
+++ app/views/html_readme.js
@@ -8,19 +8,20 @@ module.exports = BaseView.extend({
   },
   update: function (open) {
     if(!open)
-    	return;
-    var model = this.model.contents.findWhere({
-    	name: 'README.md'
-    });
+      return;
     var el = this.el;
+    var model = this.model.contents.find(function(data){
+      return data.get('name') && data.get('name').toLowerCase() === 'readme.md';
+    });
+
     if(!model){
       el.innerHTML = '<h1>Add a README.md</h1>';
-    	return;
+      return;
     }
     marked(model.get('content'), function (err, html) {
-    	if(err)
-    		return;
-    	el.innerHTML = html;
+      if(err)
+        return;
+      el.innerHTML = html;
     });
   }
 });
