9c79b209f361773ccec8602cf7ccabf5673a99d8
Casey Flynn <cflynn.us@gmail.com>
2014-02-18 14:43:25 -0800

    PC-48 incremental work on refactoring markdown render

--- app/views/html_readme.js
+++ app/views/html_readme.js
@@ -4,26 +4,36 @@ var BaseView = require('./base_view'),
 module.exports = BaseView.extend({
   id: 'html-readme',
   postHydrate: function () {
-    this.app.dispatch.on('toggle:readme', this.update, this);
+    this.app.dispatch.on('toggle:readme', this.toggle, this);
   },
-  update: function (open) {
-    if(!open)
-      return;
-    var el = this.el;
-    var model = this.model.contents.find(function(data){
+  toggle: function (open) {
+    this.options.open = open;
+    this.render();
+  },
+  getTemplateData: function () {
+
+    var opts = this.options;
+    var readmeFile = this.model.contents.find(function(data){
       return data.get('name') && data.get('name').toLowerCase() === 'readme.md';
     });
-    if(!model){
-      el.innerHTML = '<h1>Add a README.md</h1>';
-      return;
+
+    if (!readmeFile) {
+      if (this.options.editmode) { // container page
+        opts.html = '';
+      }
+      else { 
+        // show blank;
+        opts.html = '';
+      }
     }
-    marked(model.get('content'), function (err, html) {
-      if(err)
-        return;
-      el.innerHTML = html;
-    });
+    else {
+      opts.html = marked(readmeFile.get('content'));
+    }
+
+    return opts;
+
   },
-  postRender: function () {
+  preRender: function () {
     if(this.collection.length === 0){
       this.app.dispatch.trigger('toggle:readme', true);
     }
