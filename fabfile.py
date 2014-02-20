3172376df138683f4bd667bd8e19cffb292c0098
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-02-20 15:17:38 -0800

    add staging to fab

--- fabfile.py
+++ fabfile.py
@@ -8,6 +8,15 @@ env.use_ssh_config = True
 """
 Environments
 """
+def staging():
+  """
+  Work on staging environment
+  """
+  env.settings = 'staging'
+  env.hosts = [
+    'web-rep_int',
+  ]
+
 def production():
   """
   Work on production environment
@@ -21,7 +30,7 @@ def production():
 
 def integration():
   """
-  Work on staging environment
+  Work on integration environment
   """
   env.settings = 'integration'
   env.hosts = [
@@ -58,11 +67,14 @@ def setup():
   """
   Install and start the server.
   """
-  require('settings', provided_by=[production, integration])
+  require('settings', provided_by=[production, integration, staging])
   require('branch', provided_by=[stable, master, branch])
 
   install_node()
   clone_repo()
+  run('echo before checkin')
+  checkinBranch()
+  run('echo after checkin')
   # checkout_latest()
   install_requirements()
   bower()
@@ -100,12 +112,12 @@ def install_requirements():
   """
   Install the required packages using npm.
   """
-  sudo('npm install --registry http://npm.nodejs.org.au:5984/registry/_design/app/_rewrite pm2 grunt-cli bower -g')
+  sudo('npm install pm2 grunt-cli bower -g')
   sudo('apt-get install -y rubygems')
   sudo('gem install compass')
   sudo('rm -rf ~/tmp')
   with cd('runnable-web'):
-    run('npm install --registry http://npm.nodejs.org.au:5984/registry/_design/app/_rewrite')
+    run('npm install')
 
 def bower():
   with cd('runnable-web'):
@@ -131,10 +143,13 @@ def deploy():
   """
   Deploy the latest version of the site to the server.
   """
-  require('settings', provided_by=[production, integration])
+  require('settings', provided_by=[production, integration, staging])
   require('branch', provided_by=[stable, master, branch])
 
   checkout_latest()
+  run('echo before checkin; pwd')
+  checkinBranch()
+  run('echo after checkin; pwd')
   install_requirements()
   bower()
   grunt()
@@ -157,7 +172,7 @@ def rollback(commit_id):
   There is NO guarantee we have committed a valid dataset for an arbitrary
   commit hash.
   """
-  require('settings', provided_by=[production, integration])
+  require('settings', provided_by=[production, integration, staging])
   require('branch', provided_by=[stable, master, branch])
 
   checkout_latest()
