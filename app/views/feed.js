6f3b9c5bcb93d41797b167e156e1a32b75e8099b
Casey Flynn <cflynn.us@gmail.com>
2014-03-10 16:24:57 -0700

    BC-10 implementing client-side toggle popular/trending feeds

--- app/views/feed.js
+++ app/views/feed.js
@@ -2,7 +2,15 @@ var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
   tagName: 'section',
-  id: 'feed'
+  id: 'feed',
+  postRender: function () {
+    //triggered from feed_toggles.js
+    this.app.dispatch.on('toggle:toggleFeed', function (data) {
+      this.$el.find('#feed_trending_container').hide();
+      this.$el.find('#feed_popular_container').hide();
+      this.$el.find('#feed_' + data + '_container').show();
+    }, this);
+  }
 });
 
 module.exports.id = "Feed";
