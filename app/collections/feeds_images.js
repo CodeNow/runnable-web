9246ce6d7c100f12b3fae828fcb6b45125623712
Casey Flynn <cflynn.us@gmail.com>
2014-03-10 11:45:42 -0700

    BC-10 merge

--- app/collections/feeds_images.js
+++ app/collections/feeds_images.js
@@ -3,7 +3,7 @@ var Runnables = require('./runnables');
 
 module.exports = Runnables.extend({
   model: Image,
-  url: 'feeds/images'
+  url: '/feeds/images'
 });
 
 module.exports.id = "FeedsImages";
