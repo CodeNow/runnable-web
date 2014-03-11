d700da22b069df72fac3f10561512207e4e51213
Casey Flynn <cflynn.us@gmail.com>
2014-03-11 11:49:20 -0700

    BC-10 rendering non-active page views as display:none on page load

--- app/views/feed.js
+++ app/views/feed.js
@@ -1,8 +1,24 @@
 var BaseView = require('./base_view');
+var utils = require('../utils');
 
 module.exports = BaseView.extend({
   tagName: 'section',
   id: 'feed',
+  getTemplateData: function () {
+    var opts = this.options;
+    opts.orderBy = utils.getQueryParam(this.app, 'orderBy');
+    if(!opts.orderBy)
+      opts.orderBy = 'trending'; //default
+
+    if(opts.orderBy == 'trending'){
+      opts.orderByTrending = true;
+      opts.orderByPopular = false;
+    }else{
+      opts.orderByTrending = false;
+      opts.orderByPopular = true;
+    }
+    return opts;
+  },
   postRender: function () {
     //triggered from feed_toggles.js
     this.app.dispatch.on('toggle:toggleFeed', function (data) {
--- app/views/feed_toggles.js
+++ app/views/feed_toggles.js
@@ -1,14 +1,31 @@
 var BaseView = require('./base_view');
+var queryString = require('query-string');
 
 module.exports = BaseView.extend({
   className: 'btn-group',
   events: {
     'click button' : 'toggleFeed'
   },
+  getTemplateData: function () {
+    var opts = this.options;
+    if(opts.defaultActive == 'popular'){
+      opts.activeTrending = false;
+      opts.activePopular = true;
+    }else{
+      opts.activeTrending = true;
+      opts.activePopular = false;
+    }
+    return opts;
+  },
   toggleFeed: function (evt) {
     this.$('button').toggleClass('active');
+    var id = this.$(evt.currentTarget).attr('id');
     if(this.app.dispatch)
-      this.app.dispatch.trigger('toggle:toggleFeed', this.$(evt.currentTarget).attr('id'));
+      this.app.dispatch.trigger('toggle:toggleFeed', id);
+
+    var qs = queryString.parse(location.search);
+    qs.orderBy = id;
+    window.history.pushState('', '', window.location.pathname + '?' + queryString.stringify(qs));
   }
 });
 
