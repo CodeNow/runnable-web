3aa6e216f0531e915201b84a84a8308a8bc1e93a
nommer <tli415@gmail.com>
2014-03-13 13:05:24 -0700

    BC-10 hide filters over 16

--- app/views/feed_filters.js
+++ app/views/feed_filters.js
@@ -6,30 +6,38 @@ module.exports = BaseView.extend({
   id: 'filters',
   className: 'col-md-2 col-sm-3',
   events: {
-    'click li' : 'filterItem'
-  },
-  filterItem: function (evt) {
-    var self = this;
-    var $h3 = self.$('h3');
-
-    // mark as active
-    self.$(evt.currentTarget).toggleClass('active');
-
-    // add 'ing' to 'filter' and show clear
-    if (self.$('li').hasClass('active')) {
-      $h3.removeClass('out').addClass('in');
-    } else {
-      $h3.addClass('out').removeClass('in');
-    }
-
+    'click .show-more'    : 'showMore',
+    'click li'            : 'filterItem'
   },
   getTemplateData: function () {
-
     var opts = this.options;
     //var queryObj = utils.getCurrentUrlQueryString(this.app);
     //console.log(queryObj);
     return opts;
+  },
+  showMore: function (evt) {
+    var $ol = this.$('ol');
 
+    if (!$ol.hasClass('in')) {
+      $ol.addClass('in');
+    }
+  },
+  filterItem: function (evt) {
+    // if li is not :last-child
+    if (!$(evt.currentTarget).hasClass('show-more')) {
+      var self = this;
+      var $h3 = self.$('h3');
+
+      // mark as active
+      self.$(evt.currentTarget).toggleClass('active');
+
+      // add 'ing' to 'filter' and show clear
+      if (self.$('li').hasClass('active')) {
+        $h3.removeClass('out').addClass('in');
+      } else {
+        $h3.addClass('out').removeClass('in');
+      }
+    }
   }
 });
 
