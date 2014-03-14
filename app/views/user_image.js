5dc80ac66a3b9d62352653063078f20e3e336043
nommer <tli415@gmail.com>
2014-03-13 18:59:24 -0700

    BC-10 added links to user-img

--- app/views/user_image.js
+++ app/views/user_image.js
@@ -1,6 +1,17 @@
 var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
+  tagName: 'a',
+  className: 'user-img',
+  preRender: function () {
+    if (this.model) {
+      var username = this.model.attributes.username;
+
+      this.attributes = {
+        href: '/u/' + username
+      };
+    }
+  }
 });
 
 module.exports.id = "UserImage";
