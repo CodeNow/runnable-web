03157a10e95630a7ae98e0be59560415a0257e7d
nommer <tli415@gmail.com>
2014-03-24 14:04:34 -0700

    BC-10 remove rows
    were causing issues in ie11

--- app/views/channel_cards.js
+++ app/views/channel_cards.js
@@ -2,7 +2,8 @@ var BaseView = require('./base_view');
 var Channel = require('../models/channel');
 
 module.exports = BaseView.extend({
-  id: 'cards'
+  id: 'cards',
+  className: 'row'
   // getTemplateData: function () {
   //   if (this.options.basehref == '/new') {
   //     this.collection.insert(0, new Channel({ name:'runnableid' }));
