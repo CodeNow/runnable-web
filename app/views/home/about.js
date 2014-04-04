b96ef4a4fdb00c6e2a53e68500c894e8cafe72b3
Casey Flynn <cflynn.us@gmail.com>
2014-04-03 17:03:38 -0700

    LFU-8 fixing contact form

--- app/views/home/about.js
+++ app/views/home/about.js
@@ -1,24 +1,35 @@
 var BaseView = require('../base_view');
+var feedbackThanks = require('../modals/feedback_thanks');
 
 module.exports = BaseView.extend({
   id:'about',
+  events: {
+    'submit form': 'submit'
+  },
+  submit: function (evt) {
+    evt.preventDefault();
+    this.$(evt.currentTarget)[0].reset();
+    debugger;
+    this.$.post('/api/-', this.$(evt.currentTarget)[0].stingify(), function () { /* we don't care */ });
+    var FeedbackThanksModal = new feedbackThanks({ app:this.app });
+    FeedbackThanksModal.open();
+  },
   postRender: function () {
     setTimeout(function () {
       $.stellar();
     }, 100); // timeout for clientside hit, else doesnt work..
-
     // Getting images embedded in an SVG to show up in Safari...
     // http://stackoverflow.com/questions/12513308/svg-used-as-background-image-loses-embed-images
     function loadSVG(svgpath){
-         if( /webkit/gi.test(navigator.userAgent.toLowerCase()) ){
-           var obj = document.createElement("object");
-           obj.setAttribute("type", "image/svg+xml");
-           obj.setAttribute("data", svgpath);
-           obj.setAttribute("width", "1");
-           obj.setAttribute("height", "1");
-           obj.setAttribute("style", "width: 0px; height: 0px; position: absolute; visibility: hidden");
-           document.getElementsByTagName("html")[0].appendChild(obj);
-         }
+      if( /webkit/gi.test(navigator.userAgent.toLowerCase()) ){
+        var obj = document.createElement("object");
+        obj.setAttribute("type", "image/svg+xml");
+        obj.setAttribute("data", svgpath);
+        obj.setAttribute("width", "1");
+        obj.setAttribute("height", "1");
+        obj.setAttribute("style", "width: 0px; height: 0px; position: absolute; visibility: hidden");
+        document.getElementsByTagName("html")[0].appendChild(obj);
+      }
     }
     window.onload = function(){
       loadSVG("/images/about-press.svg");
