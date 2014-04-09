f4ae3314b36ba013f75654a47b38bcc37794aaee
tjmehta <tj@init.me>
2014-04-08 19:41:37 -0700

    internal clustering, memory leak bandaid

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
 /*jshint strict:false */
 require('console-trace')({ always:true });
+var os = require('os');
 var config = require('./server/lib/env').current;
-var server = require('./server/server');
-if (config.newrelic) {
-  require('newrelic');
-}
-var port = config.port;
 
-server.init({}, function(err) {
-  if (err) throw err;
-  server.start({port: port}, function(err){
-    if (err) { console.log(err); }
+var cluster = require('cluster');
+
+var workers = [];
+if (cluster.isMaster) {
+  startMonitoring();
+  os.cpus().forEach(function () {
+    createWorker(); // create 2 workers per core..
+    createWorker();
   });
-});
+  memoryLeakPatch();
+}
+else {
+  var server = require('./server/server');
+  var port = config.port;
+
+  server.init({}, function(err) {
+    if (err) throw err;
+    server.start({port: port}, function(err){
+      if (err) { console.log(err); }
+    });
+  });
+}
+
+function createWorker () {
+  var worker = cluster.fork();
+  workers.push(worker);
+  console.log('Creating new worker', worker.id);
+  return worker;
+}
+
+function startMonitoring () {
+  if (process.env.NODE_ENV === 'production' && config.nodetime) {
+    var nodetime = require('nodetime');
+    nodetime.profile(config.nodetime);
+  }
+  if (config.newrelic) {
+    require('newrelic');
+  }
+}
+
+function memoryLeakPatch () {
+  // memory leak patch! - start restart timeout
+  var numWorkers = os.cpus().length * 2;
+  var restartTime  = 10*1000;
+  var maxDrainTime = 30 * 1000;
+  setInterval(killAndStartNewWorker, restartTime/numWorkers);
+  function killAndStartNewWorker (message) {
+    var w = workers.shift();
+    createWorker();
+
+    console.log('Kill old worker', w.id);
+    setTimeout(w.kill.bind(w), maxDrainTime);
+    w.disconnect();
+  }
+}
