b9b77e15fdb67e328992158651ccda002ee5d780
tjmehta <tj@init.me>
2014-04-14 14:24:09 -0700

    cluster recover on worker exit

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 require('console-trace')({ always:true });
 var os = require('os');
 var config = require('./server/lib/env').current;
-
+var pluck = require('map-utils').pluck;
 var cluster = require('cluster');
 
-var workers = [];
+var workers;
 if (cluster.isMaster) {
+  workers = [];
   startMonitoring();
   os.cpus().forEach(function () {
     createWorker(); // create 2 workers per core..
     createWorker();
   });
   memoryLeakPatch();
+  handleWorkerExits();
 }
 else {
   var server = require('./server/server');
@@ -24,6 +26,8 @@ else {
       if (err) { console.log(err); }
     });
   });
+
+  handleUncaughtExceptions();
 }
 
 function createWorker () {
@@ -57,4 +61,34 @@ function memoryLeakPatch () {
     setTimeout(w.kill.bind(w), maxDrainTime);
     w.disconnect();
   }
+}
+
+function handleWorkerExits () {
+  cluster.on('exit', function (worker, code) {
+    console.error('Worker exited', worker.id, 'with code:', code);
+    workers.map(pluck('id')).some(function (workerId, i) {
+      if (workerId === worker.id) {
+        workers.splice(i, 1); // remove worker from workers
+        return true;
+      }
+    });
+    if (code !== 0) { // dont restart if successful exit.
+      createWorker();
+    }
+  });
+}
+
+function handleUncaughtExceptions () {
+  process.on('uncaughtException', function (err) {
+    if (err.message) console.log(err.message);
+    if (err.stack) {
+      console.log(err.stack);
+    }
+    else {
+      var e = new Error('debug');
+      console.log('no error stack - debug stack');
+      console.log(e.stack);
+    }
+    process.exit(1);
+  });
 }
