1dc5a099a424563b296317d8f4113acbb4816ce6
runnabro <tli415@gmail.com>
2014-04-21 16:19:17 -0700

    add shortcut popover and update popovers

--- app/views/add_tab.js
+++ app/views/add_tab.js
@@ -1,7 +1,24 @@
 var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
-  id: 'add-tab'
+  tagName: 'div',
+  id: 'add-tab',
+  events: {
+    'click' : 'togglePopover'
+  },
+  togglePopover: function () {
+    var $self = this.$el;
+    var $popover = this.$('.popover');
+
+    if ($self.hasClass('active')) {
+      $self.removeClass('active');
+      $popover.removeClass('in');
+    }
+    else {
+      $self.addClass('active');
+      $popover.addClass('in');
+    }
+  }
 });
 
 module.exports.id = "AddTab";
