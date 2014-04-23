9a8250db0f26fdfd3220127d03b78ba0deac56d5
Casey Flynn <cflynn.us@gmail.com>
2014-04-23 14:28:45 -0700

    removing unused collection

--- app/collections/open_tabs.js
+++
@@ -1,9 +0,0 @@
-var Model = require('../models/fs');
-var Base = require('./base');
-
-module.exports = Base.extend({
-  model: Model,
-  initialize: function () {}
-});
-
-module.exports.id = 'OpenTabs';
