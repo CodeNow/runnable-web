163bb7f20a97b69f3a8a70e58a9eb44188818844
Casey Flynn <cflynn.us@gmail.com>
2014-04-25 13:46:29 -0700

    private property toggling UI

--- app/views/public_toggle.js
+++ app/views/public_toggle.js
@@ -1,7 +1,27 @@
 var BaseView = require('./base_view');
+var _ = require('underscore');
+var utils = require('../utils');
 
 module.exports = BaseView.extend({
-  className: 'btn-group toggles'
+  className: 'btn-group toggles',
+  events: {
+    'click [data-action]': 'toggle'
+  },
+  toggle: function (evt) {
+    var $el = $(evt.currentTarget);
+    var value = ($el.attr('data-value') === 'true');
+    var cb = function () {
+      this.render();
+    };
+    cb = utils.cbOpts(cb, this);
+    this.model.save({
+      'private': value
+    }, cb);
+  },
+  getTemplateData: function () {
+    var opts = this.options;
+    return opts;
+  }
 });
 
 module.exports.id = "PublicToggle";
