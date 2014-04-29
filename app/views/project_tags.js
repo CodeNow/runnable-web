0bda41b5dfa00bd1e8f5fda2feb822a0cc7539f3
Casey Flynn <cflynn.us@gmail.com>
2014-04-29 13:11:16 -0700

    refactoring tags

--- app/views/project_tags.js
+++ app/views/project_tags.js
@@ -1,12 +1,18 @@
 var BaseView = require('./base_view');
+var _ = require('underscore');
 
 module.exports = BaseView.extend({
   id: 'project_tags',
-  attributes: {
-    style: 'display: inline-block;' //TODO: Talk to tony and make this not inline css
-  },
+  tagName: 'ul',
+  className: 'tags',
   postHydrate: function () {
     this.listenTo(this.model, 'change:tags', this.render.bind(this));
+  },
+  getTemplateData: function () {
+    var opts = this.options;
+    return _.extend(opts, {
+      isVerified: this.app.user.isVerified()
+    });
   }
 });
 
