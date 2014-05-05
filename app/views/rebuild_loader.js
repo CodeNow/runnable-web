dfc356b7c6806b3d9190e8c1ff0266b3865bec4e
runnabro <tli415@gmail.com>
2014-05-05 12:18:31 -0700

    PC-87 add cancel to rebuild loader

--- app/views/rebuild_loader.js
+++ app/views/rebuild_loader.js
@@ -2,7 +2,13 @@ var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
   id: 'rebuild-loader',
-  className: 'with-text'
+  className: 'with-text',
+  events: {
+    'click a' : 'cancel'
+  },
+  cancel: function () {
+    $('#project').removeClass('out');
+  }
 });
 
 module.exports.id = "RebuildLoader";
