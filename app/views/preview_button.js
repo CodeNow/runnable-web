d5252ae3d12c643b2416d7c00b302bb791166a4d
runnabro <tli415@gmail.com>
2014-05-05 17:22:06 -0700

    PC-87 preview wip

--- app/views/preview_button.js
+++ app/views/preview_button.js
@@ -2,7 +2,17 @@ var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
   tagName: 'button',
-  className: 'purple'
+  className: 'purple',
+  events: {
+    'click' : 'enterPreview'
+  },
+  enterPreview: function () {
+    // changes status bar text
+    $('#runnable').addClass('preview');
+
+    // preview needs to be rebuilt, show loader
+    $('#project').addClass('out');
+  }
 });
 
 module.exports.id = "PreviewButton";
