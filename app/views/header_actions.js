3574c8ba7be1e7c08132100ca16c8918d8588804
Casey Flynn <cflynn.us@gmail.com>
2014-05-12 12:12:02 -0700

    updating modal stub for tony

--- app/views/header_actions.js
+++ app/views/header_actions.js
@@ -1,14 +1,14 @@
 var BaseView = require('./base_view');
 var LoginModal = require('./login_modal');
-var SignupModal = require('./signup_modal');
 var PublishRequestModal = require('./publish_request_modal');
+var RegisterModal = require('./modals/register_modal');
 
 module.exports = BaseView.extend({
   id: 'header-actions',
   className: 'col-sm-4',
   events: {
-    'click #header-login-link'  : 'openLogin',
-    'click #header-signup-link' : 'openSignup'
+    'click #header-login-link'  :     'openLogin',
+    'click #header-signup-link' :     'openSignup'
   },
   postHydrate: function () {
     this.listenTo(this.model, 'change:username', this.render.bind(this));
@@ -20,8 +20,12 @@ module.exports = BaseView.extend({
   },
   openSignup: function (evt) {
     evt.preventDefault();
-    var signupModal = new SignupModal({ app:this.app });
-    signupModal.open();
+    var registerModal = new RegisterModal({ app:this.app });
+    registerModal.open();
+  },
+  openSignupNew: function (evt) {
+    var registerModal = new RegisterModal({app: this.app});
+    registerModal.open();
   }
 });
 
