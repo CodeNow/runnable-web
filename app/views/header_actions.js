23c7f4e45bc933eb8fa7acb05cda7e2677231824
Casey Flynn <cflynn.us@gmail.com>
2014-05-13 15:44:43 -0700

    fix deps for header_actions.js module

--- app/views/header_actions.js
+++ app/views/header_actions.js
@@ -1,20 +1,15 @@
 var BaseView = require('./base_view');
 var PublishRequestModal = require('./publish_request_modal');
+var RegisterModal = require('./modals/register_modal');
 
 module.exports = BaseView.extend({
   id: 'header-actions',
   className: 'col-sm-4',
   events: {
-    'click #header-login-link'  :     'openLogin',
     'click #header-signup-link' :     'openSignup'
   },
   postHydrate: function () {
     this.listenTo(this.model, 'change:username', this.render.bind(this));
-  },
-  openLogin: function (evt) {
-    evt.preventDefault();
-    var loginModal = new LoginModal({ app:this.app });
-    loginModal.open();
   },
   openSignup: function (evt) {
     evt.preventDefault();
