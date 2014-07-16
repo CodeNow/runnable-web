211ea3da13831e4729e5309cbe29400f8fcbc143
Ben Plunkert <ben@init.me>
2014-07-16 15:30:55 -0700

    normal command for npm run build in chef cookbook

--- cookbook/recipes/deploy.rb
+++ cookbook/recipes/deploy.rb
@@ -42,8 +42,8 @@ execute 'bower install' do
 end
 
 execute 'npm run build' do
-  command "sh -c 'npm run build | grep \"Done, without errors.\"'"
-  environment {'PATH' => 'PATH:/opt/chef/embedded/bin'}
+  command 'npm run build'
+  environment({'PATH' => 'PATH:/opt/chef/embedded/bin'})
   cwd "#{node['runnable_web']['deploy_path']}/current"
   creates "#{node['runnable_web']['deploy_path']}/current/public/styles/index.css"
   action :nothing
