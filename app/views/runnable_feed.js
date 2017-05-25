b6ddc1cf0a1f6ef6c9ea51e9f4e3bd6a1e555e16
Shivam Bajpai <shivam.bajpai@stackexpress.com>
2017-05-24 23:09:11 -0700

    ADD: soft delete views

--- app/views/runnable_feed.js
+++ app/views/runnable_feed.js
@@ -1,8 +1,17 @@
 var BaseView = require('./base_view');
 
 module.exports = BaseView.extend({
+
   tagName: 'ul',
-  className: 'col-md-10 col-sm-9 runnable-feed'
+  className: 'col-md-10 col-sm-9 runnable-feed',
+  events: {
+  },
+  postRender: function () {
+  },
+  getTemplateData: function () {
+    return this.options;
+  }
+
 });
 
 module.exports.id = "RunnableFeed";
